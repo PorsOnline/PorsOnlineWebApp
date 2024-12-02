74f75d256479e59f3ec56f3b14dbaf6bf7a9a884
Mohammad-Javad Rezaei <javad@Javads-MacBook-Pro.local>
2024-12-02 23:23:38 +0330

    fix: fix indent

--- cmd/votingSystem/main.go
+++ cmd/votingSystem/main.go
@@ -19,19 +19,17 @@ func main() {
 
 	if v := os.Getenv("CONFIG_PATH"); len(v) > 0 {
 		*configPath = v
-
-		c := config.MustReadConfig(*configPath)
-
-		err := logger.InitLogger(c)
-		if err != nil {
-			log.Fatal("can not initialize logger")
-		}
-		logger.Info("Starting the program", nil)
-		appContainer := app.NewMustApp(c)
-		err = http.Run(appContainer, c)
-		if err != nil {
-			log.Fatal("can not start the programm")
-		}
-
 	}
+	c := config.MustReadConfig(*configPath)
+	err := logger.InitLogger(c)
+	if err != nil {
+		log.Fatal("can not initialize logger")
+	}
+	logger.Info("Starting the program", nil)
+	appContainer := app.NewMustApp(c)
+	err = http.Run(appContainer, c)
+	if err != nil {
+		log.Fatal("can not start the programm")
+	}
+
 }
