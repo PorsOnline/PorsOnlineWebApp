74ad76aabb4c0cb315171d6481e7090554ab7226
RaziyeNikookolah <r.nikookolah@gmail.com>
2024-12-02 20:44:15 +0330

    feat: impl createUser and getUserByID in service.go

---
+++ internal/user/service.go
@@ -0,0 +1,55 @@
+package user
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/porseOnline/internal/user/domain"
+	"github.com/porseOnline/internal/user/port"
+)
+
+var (
+	ErrUserOnCreate           = errors.New("error on creating new user")
+	ErrUserCreationValidation = errors.New("validation failed")
+	ErrUserNotFound           = errors.New("user not found")
+)
+
+type service struct {
+	repo port.Repo
+}
+
+func NewService(repo port.Repo) port.Service {
+	return &service{
+		repo: repo,
+	}
+}
+
+func (s *service) CreateUser(ctx context.Context, user domain.User) (domain.UserID, error) {
+	if err := user.Validate(); err != nil {
+		return 0, fmt.Errorf("%w %w", ErrUserCreationValidation, err)
+	}
+
+	userID, err := s.repo.Create(ctx, user)
+	if err != nil {
+		log.Println("error on creating new user : ", err.Error())
+		return 0, ErrUserOnCreate
+	}
+
+	return userID, nil
+}
+
+func (s *service) GetUserByID(ctx context.Context, userID domain.UserID) (*domain.User, error) {
+	user, err := s.repo.GetByID(ctx, userID)
+	if err != nil {
+		print("errrrrror")
+		return nil, err
+	}
+
+	if user.ID == 0 {
+		return nil, ErrUserNotFound
+	}
+
+	return user, nil
+}
