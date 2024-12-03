983e5be73556071b4fdafda843dd09ef6b8b7877
RaziyeNikookolah <r.nikookolah@gmail.com>
2024-12-03 22:36:21 +0330

    feat: add sendEmail func in helper pkg

--- pkg/helper/helper.go
+++ pkg/helper/helper.go
@@ -1,9 +1,11 @@
 package helper
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
+	"gopkg.in/gomail.v2"
 )
 
 func GetConfig(key string) string {
@@ -14,4 +16,21 @@ func LoadEnvFile() {
 	if err != nil {
 		panic("Error loading .env file")
 	}
+}
+
+func SendEmail(to string) {
+	m := gomail.NewMessage()
+	m.SetHeader("From", "r.nikookolah@gmail.com")
+	m.SetHeader("To", to)
+	m.SetHeader("Subject", "PosOnline verification code.")
+	m.SetBody("text/plain", "Now you can try to send code .")
+
+	// SMTP server configuration
+	d := gomail.NewDialer("smtp.gmail.com", 587, "ixjd oort uizn pdee", to)
+
+	// Send the email
+	if err := d.DialAndSend(m); err != nil {
+		fmt.Println("Not send.")
+	}
+
 }
