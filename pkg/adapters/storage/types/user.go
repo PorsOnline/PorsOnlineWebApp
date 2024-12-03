0cf9d1dd9c721d72a591c436c6a16cfe1565e8b7
RaziyeNikookolah <r.nikookolah@gmail.com>
2024-12-03 22:44:28 +0330

    feat: complete user struct fields with options uniq and default

--- pkg/adapters/storage/types/user.go
+++ pkg/adapters/storage/types/user.go
@@ -10,13 +10,13 @@ type User struct {
 	gorm.Model
 	FirstName         string    `gorm:"column:first_name"`
 	LastName          string    `gorm:"column:last_name"`
-	Phone             string    `gorm:"column:phone"`
-	Email             string    `gorm:"column:email"`
+	Phone             string    `gorm:"column:phone;unique"`
+	Email             string    `gorm:"column:email;unique"`
 	PasswordHash      string    `gorm:"column:password_hash"`
-	NationalCode      string    `gorm:"column:national_code"`
+	NationalCode      string    `gorm:"column:national_code;unique"`
 	BirthDate         time.Time `gorm:"column:birth_date"`
 	City              string    `gorm:"column:city"`
-	Gender            string    `gorm:"column:gender"`
-	SurveyLimitNumber int       `gorm:"column:survey_limit_number"`
-	Balance           int       `gorm:"column:balance"`
+	Gender            bool      `gorm:"column:gender;default:true"`
+	SurveyLimitNumber int       `gorm:"column:survey_limit_number;default:100"`
+	Balance           int       `gorm:"column:balance;default:100"`
 }
