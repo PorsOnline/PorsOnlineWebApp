b971cac15dffbf07a5da6ca8fbfe37e4dc36b4b3
RaziyeNikookolah <r.nikookolah@gmail.com>
2024-12-03 23:06:27 +0330

    send email verified

--- pkg/helper/helper.go
+++ pkg/helper/helper.go
@@ -19,18 +19,21 @@ func LoadEnvFile() {
 }
 
 func SendEmail(to string) {
+	LoadEnvFile()
+	GOOGLE_SMTP_PASSWORD := GetConfig("GOOGLE_SMTP_PASSWORD")
 	m := gomail.NewMessage()
 	m.SetHeader("From", "r.nikookolah@gmail.com")
 	m.SetHeader("To", to)
 	m.SetHeader("Subject", "PosOnline verification code.")
-	m.SetBody("text/plain", "Now you can try to send code .")
+	m.SetBody("text/plain", "Now you can try to send the code.")
 
 	// SMTP server configuration
-	d := gomail.NewDialer("smtp.gmail.com", 587, "ixjd oort uizn pdee", to)
+	d := gomail.NewDialer("smtp.gmail.com", 587, "r.nikookolah@gmail.com", GOOGLE_SMTP_PASSWORD)
 
 	// Send the email
 	if err := d.DialAndSend(m); err != nil {
-		fmt.Println("Not send.")
+		fmt.Println("Failed to send email:", err)
+	} else {
+		fmt.Println("Email sent successfully.")
 	}
-
 }
