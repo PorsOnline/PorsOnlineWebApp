a7bc06876f7a5ce918467edf89a6f6b8ab10d45f
RaziyeNikookolah <r.nikookolah@gmail.com>
2024-12-03 14:38:36 +0330

    fix: balance spell and gorm notation

--- pkg/adapters/storage/types/user.go
+++ pkg/adapters/storage/types/user.go
@@ -8,18 +8,15 @@ import (
 
 type User struct {
 	gorm.Model
-	FirstName    string
-	LastName     string
-	Phone        string
-	Email        string
-	PasswordHash string
-	NationalCode string
-
-	BirthDate         time.Time
-	City              string
-	Gender            string
-	SurveyLimitNumber int
-	Create_at         time.Time
-	Update_at         time.Time
-	Balane            int
+	FirstName         string    `gorm:"column:first_name"`
+	LastName          string    `gorm:"column:last_name"`
+	Phone             string    `gorm:"column:phone"`
+	Email             string    `gorm:"column:email"`
+	PasswordHash      string    `gorm:"column:password_hash"`
+	NationalCode      string    `gorm:"column:national_code"`
+	BirthDate         time.Time `gorm:"column:birth_date"`
+	City              string    `gorm:"column:city"`
+	Gender            string    `gorm:"column:gender"`
+	SurveyLimitNumber int       `gorm:"column:survey_limit_number"`
+	Balance           int       `gorm:"column:balance"`
 }
