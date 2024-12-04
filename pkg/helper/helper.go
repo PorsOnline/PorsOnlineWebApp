487ba8b2ebeedcfd0bb64bbf52e6be46ec6a267c
RaziyeNikookolah <r.nikookolah@gmail.com>
2024-12-04 08:56:41 +0330

    feat: add GetRandomCode func

--- pkg/helper/helper.go
+++ pkg/helper/helper.go
@@ -2,7 +2,9 @@ package helper
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"gopkg.in/gomail.v2"
@@ -18,14 +20,15 @@ func LoadEnvFile() {
 	}
 }
 
-func SendEmail(to string) {
+func SendEmail(to string, message string) {
 	LoadEnvFile()
 	GOOGLE_SMTP_PASSWORD := GetConfig("GOOGLE_SMTP_PASSWORD")
 	m := gomail.NewMessage()
 	m.SetHeader("From", "r.nikookolah@gmail.com")
 	m.SetHeader("To", to)
 	m.SetHeader("Subject", "PosOnline verification code.")
-	m.SetBody("text/plain", "Now you can try to send the code.")
+	bodyMessage := fmt.Sprintf("Now you can try to send the code %s", message)
+	m.SetBody("text/plain", bodyMessage)
 
 	// SMTP server configuration
 	d := gomail.NewDialer("smtp.gmail.com", 587, "r.nikookolah@gmail.com", GOOGLE_SMTP_PASSWORD)
@@ -36,4 +39,10 @@ func SendEmail(to string) {
 	} else {
 		fmt.Println("Email sent successfully.")
 	}
+}
+func GetRandomCode() int {
+	rand.Seed(time.Now().UnixNano())
+
+	// Generate a random 5-digit number (10000 to 99999)
+	return rand.Intn(90000) + 10000
 }
