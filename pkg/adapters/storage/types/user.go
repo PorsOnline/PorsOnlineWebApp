0959d0299c1d32905fdf71a73e9e144e9026691b
RaziyeNikookolah <r.nikookolah@gmail.com>
2024-12-05 17:08:07 +0330

    chore: add not null feature to user field

--- pkg/adapters/storage/types/user.go
+++ pkg/adapters/storage/types/user.go
@@ -10,13 +10,13 @@ type User struct {
 	gorm.Model
 	FirstName         string    `gorm:"column:first_name"`
 	LastName          string    `gorm:"column:last_name"`
-	Phone             string    `gorm:"column:phone;unique"`
-	Email             string    `gorm:"column:email;unique"`
-	PasswordHash      string    `gorm:"column:password_hash"`
-	NationalCode      string    `gorm:"column:national_code;unique"`
+	Phone             string    `gorm:"column:phone;unique;not null"`
+	Email             string    `gorm:"column:email;unique;not null"`
+	PasswordHash      string    `gorm:"column:password_hash;not null"`
+	NationalCode      string    `gorm:"column:national_code;unique;not null"`
 	BirthDate         time.Time `gorm:"column:birth_date"`
 	City              string    `gorm:"column:city"`
-	Gender            bool      `gorm:"column:gender"`
+	Gender            bool      `gorm:"column:gender;not null"`
 	SurveyLimitNumber int       `gorm:"column:survey_limit_number;default:100"`
 	Balance           int       `gorm:"column:balance;default:100"`
 }
