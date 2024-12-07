09e50dae09d1c7915df68cecfb70ef13d7b7c846
Mohammad-Javad Rezaei <javad@Javads-MacBook-Pro.local>
2024-12-08 00:46:20 +0330

    feat(WIP): add get last answer of user in repo

--- pkg/adapters/storage/voting_repo.go
+++ pkg/adapters/storage/voting_repo.go
@@ -25,7 +25,6 @@ func NewVotingRepo(db *gorm.DB, secretDB *gorm.DB) votingPort.Repo {
 		db:       db,
 		secretDB: secretDB,
 	}
-
 }
 
 func (su *submitRepo) Vote(ctx context.Context, answer *domain.Vote) error {
@@ -90,4 +89,37 @@ func (su *submitRepo) Vote(ctx context.Context, answer *domain.Vote) error {
 	}
 
 	return nil
+}
+
+func (su *submitRepo) GetLastAnswer(ctx context.Context, userID uint, surveyID uint) (domain.Vote, error) {
+	var lastAnswer domain.Vote
+	var secret types.Secrets
+	err := su.secretDB.Table("secrets").Where("user_id = ? AND survey_id = ?", userID, surveyID).Find(&secret).Error
+	if err != nil {
+		return domain.Vote{}, err
+	}
+	err = su.db.Table("votes").Where("user_id = ? AND survey_id = ?", userID, surveyID).Order("question_id desc").Find(&lastAnswer).Error
+	if err != nil {
+		return domain.Vote{}, err
+	}
+	key, err := hex.DecodeString(secret.Secret)
+	if err != nil {
+		return domain.Vote{}, err
+	}
+	if lastAnswer.TextResponse != "" {
+
+		lastAnswer.TextResponse, err = helper.DecryptAES(lastAnswer.TextResponse, key)
+		if err != nil {
+			return domain.Vote{}, err
+		}
+	}
+
+	if lastAnswer.SelectedOption != "" {
+		lastAnswer.SelectedOption, err = helper.DecryptAES(lastAnswer.TextResponse, key)
+		if err != nil {
+			return domain.Vote{}, err
+		}
+	}
+	return lastAnswer, nil
+
 }
