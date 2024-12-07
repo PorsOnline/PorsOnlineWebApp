661bf64ba2591d210d63678a13dc865b6d5e3f47
RaziyeNikookolah <r.nikookolah@gmail.com>
2024-12-07 14:05:00 +0330

    fix: add db to services

--- app/app.go
+++ app/app.go
@@ -1,39 +1,99 @@
 package app
 
 import (
-	surveyPort "github.com/porseOnline/internal/survey/port"
-	"github.com/porseOnline/internal/survey"
+	"context"
+
 	"github.com/porseOnline/config"
+	"github.com/porseOnline/internal/codeVerification"
+	"github.com/porseOnline/internal/common"
+	notifPort "github.com/porseOnline/internal/notification/port"
+	"github.com/porseOnline/internal/survey"
+	surveyPort "github.com/porseOnline/internal/survey/port"
 	"github.com/porseOnline/internal/user"
 	userPort "github.com/porseOnline/internal/user/port"
 	"github.com/porseOnline/pkg/adapters/storage"
 	"github.com/porseOnline/pkg/postgres"
 
-	notifPort "github.com/porseOnline/internal/notification/port"
+	codeVerificationPort "github.com/porseOnline/internal/codeVerification/port"
 
+	"github.com/go-co-op/gocron/v2"
 	"github.com/porseOnline/internal/notification"
-
+	appCtx "github.com/porseOnline/pkg/context"
 	"gorm.io/gorm"
 )
 
 type app struct {
-	db           *gorm.DB
-	cfg          config.Config
-	userService  userPort.Service
-	notifService notifPort.Service
-	surveyService surveyPort.Service
+	db                *gorm.DB
+	cfg               config.Config
+	userService       userPort.Service
+	notifService      notifPort.Service
+	surveyService     surveyPort.Service
+	codeVrfctnService codeVerificationPort.Service
 }
 
-func (a *app) UserService() userPort.Service {
-	return a.userService
+func (a *app) DB() *gorm.DB {
+	return a.db
+}
+func (a *app) UserService(ctx context.Context) userPort.Service {
+	db := appCtx.GetDB(ctx)
+	if db == nil {
+		if a.userService == nil {
+			a.userService = a.userServiceWithDB(a.db)
+		}
+		return a.userService
+	}
+
+	return a.userServiceWithDB(db)
 }
 
-func (a *app) NotifService() notifPort.Service {
-	return a.notifService
+func (a *app) userServiceWithDB(db *gorm.DB) userPort.Service {
+	return user.NewService(storage.NewUserRepo(db))
+}
+func (a *app) NotifService(ctx context.Context) notifPort.Service {
+	db := appCtx.GetDB(ctx)
+	if db == nil {
+		if a.notifService == nil {
+			a.notifService = a.notifServiceWithDB(a.db)
+		}
+		return a.notifService
+	}
+
+	return a.notifServiceWithDB(db)
+}
+func (a *app) notifServiceWithDB(db *gorm.DB) notifPort.Service {
+	return notification.NewService(storage.NewNotifRepo(db))
 }
 
-func (a *app) SurveyService() surveyPort.Service {
-	return a.surveyService
+func (a *app) SurveyService(ctx context.Context) surveyPort.Service {
+	db := appCtx.GetDB(ctx)
+	if db == nil {
+		if a.surveyService == nil {
+			a.surveyService = a.surveyServiceWithDB(a.db)
+		}
+		return a.surveyService
+	}
+
+	return a.surveyServiceWithDB(db)
+}
+
+func (a *app) surveyServiceWithDB(db *gorm.DB) surveyPort.Service {
+	return survey.NewService(storage.NewSurveyRepo(db))
+}
+func (a *app) codeVerificationServiceWithDB(db *gorm.DB) codeVerificationPort.Service {
+	return codeVerification.NewService(
+		a.userService, storage.NewOutboxRepo(db), storage.NewCodeVerificationRepo(db))
+}
+
+func (a *app) codeVerificationService(ctx context.Context) codeVerificationPort.Service {
+	db := appCtx.GetDB(ctx)
+	if db == nil {
+		if a.codeVrfctnService == nil {
+			a.codeVrfctnService = a.codeVerificationServiceWithDB(a.db)
+		}
+		return a.codeVrfctnService
+	}
+
+	return a.codeVerificationServiceWithDB(db)
 }
 
 func (a *app) Config() config.Config {
@@ -73,8 +133,9 @@ func NewApp(cfg config.Config) (App, error) {
 	a.notifService = notification.NewService(storage.NewNotifRepo(a.db))
 
 	a.surveyService = survey.NewService(storage.NewSurveyRepo(a.db))
+	a.codeVrfctnService = codeVerification.NewService(a.userService, storage.NewOutboxRepo(a.db), storage.NewCodeVerificationRepo(a.db))
 
-	return a, nil
+	return a, a.registerOutboxHandlers()
 }
 
 func NewMustApp(cfg config.Config) App {
@@ -83,4 +144,16 @@ func NewMustApp(cfg config.Config) App {
 		panic(err)
 	}
 	return app
+}
+func (a *app) registerOutboxHandlers() error {
+	scheduler, err := gocron.NewScheduler()
+	if err != nil {
+		return err
+	}
+
+	common.RegisterOutboxRunner(a.codeVerificationServiceWithDB(a.db), scheduler)
+
+	scheduler.Start()
+
+	return nil
 }
