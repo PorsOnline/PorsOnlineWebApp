c492e1d662da5aaa2bc1e745d25e5d695f329ed8
Mohammad-Javad Rezaei <javad@Javads-MacBook-Pro.local>
2024-12-09 22:36:32 +0330

    feat: add cert and key file and using Tls protocol

--- api/handlers/http/setup.go
+++ api/handlers/http/setup.go
@@ -85,9 +85,12 @@ func Run(appContainer app.App, config config.Config) error {
 	permissionApi.Patch("/permission/:userId/validate", ValidateUserPermission(permissionService))
 	permissionApi.Patch("/permission/:permissionId/assign/:userId", AssignPermissionToUser(permissionService))
 
-  votingApi := app.Group("api/v1/vote")
+	votingApi := app.Group("api/v1/vote")
 	votingService := service.NewVotingService(appContainer.VotingService(), config.Server.Secret, config.Server.AuthExpMinute, config.Server.AuthRefreshMinute)
 	votingApi.Post("", Vote(votingService))
 
-	return app.Listen(fmt.Sprintf(":%d", config.Server.HttpPort))
+	certFile := "/PorsOnlineWebApp/certs/server.crt"
+	keyFile := "/PorsOnlineWebApp/certs/server.key"
+
+	return app.ListenTLS(fmt.Sprintf(":%d", config.Server.HttpPort), certFile, keyFile)
 }
